3e3ec7deaf260f7040badd84b928223ae41d82b5
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-28 22:44:13 +0100

    Feat(Front-end): Define routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
+import Header from "./components/Header.tsx";
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+
 const App = () => {
     return (
-        <div className="text-3xl font-bold underline">Hello world!</div>
+        <>
+            <Header/>
+            <Routes>
+                <Route path="/home" element={<HomePage/>} errorElement={<NotFoundPage/>}/>
+                <Route path="/login" element={<LoginPage/>} errorElement={<NotFoundPage/>}/>
+                <Route path="/register" element={<RegisterPage />} errorElement={<NotFoundPage/>} />
+            </Routes>
+        </>
     )
 }
 
