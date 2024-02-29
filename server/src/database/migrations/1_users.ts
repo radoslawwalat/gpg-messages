840b974d641c40367ac0a7a2de026181087edcea
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-29 08:40:42 +0100

    Update(Api): Extend public_key column length in table and refresh seed data with new entries

--- server/src/database/migrations/1_users.ts
+++ server/src/database/migrations/1_users.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
         table.increments('id').unsigned().primary().notNullable();
         table.string('username', 50).notNullable().unique();
-        table.string('public_key', 20).notNullable();
+        table.string('public_key', 5000).notNullable();
         table.string('create_date', 255).nullable();
     });
 }
