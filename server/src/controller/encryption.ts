ef7612f7c15a15600c1902d704ea07396036ce23
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2024-02-29 08:44:57 +0100

    Update(Api): Enhance encrypt controller to encrypt messages using public keys retrieved from the response, returning a hash to the user

--- server/src/controller/encryption.ts
+++ server/src/controller/encryption.ts
@@ -1,17 +1,18 @@
 import {Request, Response} from "express";
 import * as openpgp from 'openpgp';
+import Users from "../database/models/users";
 
 export const encrypt = async(req: Request, res: Response) => {
     try {
-        const {content, publicKey} = req.body;
-        const publicKeyObj = await openpgp.readKey({ armoredKey: publicKey });
-
-        const encrypted = await openpgp.encrypt({
-            message: await openpgp.createMessage({ text: content }),
-            encryptionKeys: publicKeyObj,
+        const { message, user_ids } = req.body;
+        const publicKeysArmored = await Users.getPublicKeysByIds(user_ids);
+        const encryptionKeys = await Promise.all(publicKeysArmored.map(async (armoredKey: string) => openpgp.readKey({ armoredKey })));
+        const encryptedMessage = await openpgp.encrypt({
+            message: await openpgp.createMessage({ text: message }),
+            encryptionKeys: encryptionKeys,
         });
 
-        return res.status(200).send({encryptedContent: encrypted});
+        return res.status(200).json({ encryptedMessage: encryptedMessage });
     }
     catch(error) {
         console.error("Encryption error:", error);
